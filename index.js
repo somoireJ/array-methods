dc9195101cebf2189a21cb394eba132cbba62929
Somoire Solonka <122299267+somoireJ@users.noreply.github.com>
2023-01-19 12:34:58 +0000

    addition codes on arrays methods

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const oddNumbers = numbers.filter(x => x % 2 !== 0);
 
-console.log(oddNumbers); // Output: [1, 3, 5]
+console.log(oddNumbers); 
 
 
 // The following example uses the Array.prototype.reduce() method to sum all elements in an array:
@@ -36,7 +36,7 @@ const sortedPeople = people.sort((a, b) => a.age - b.age);
 
 console.log(sortedPeople);
 // Output: [{ name: 'John', age: 20 }, { name: 'Bob', age: 25 }, { name: 'Jane', age: 30 }]
-add another longer code
+
 
 // The following example uses the Array.prototype.find() method to find the first object in an array that satisfies a given condition:
 
@@ -49,15 +49,3 @@ const people = [
 const person = people.find(person => person.age === 25);
 
 console.log(person);
-// Output: { name: 'Bob', age: 25 }
-explain all codes provided 
-
-The first code example uses the Array.prototype.map() method to create a new array with each element multiplied by 2. The Array.prototype.map() method takes a callback function as an argument and applies it to each element in the array. The callback function takes the current element as an argument and returns a new value that gets stored in the new array.
-
-The second code example uses the Array.prototype.filter() method to create a new array with only the odd numbers. The Array.prototype.filter() method takes a callback function as an argument and applies it to each element in the array. The callback function takes the current element as an argument and returns a boolean value that determines whether the element should be included in the new array.
-
-The third code example uses the Array.prototype.reduce() method to sum all elements in an array. The Array.prototype.reduce() method takes a callback function as an argument and applies it to each element in the array. The callback function takes two arguments: the accumulator (the current sum) and the current element. The callback function returns the new accumulator, which is used for the next iteration.
-
-The fourth code example uses the Array.prototype.sort() method to sort an array of objects by the value of a property. The Array.prototype.sort() method takes a callback function as an argument and applies it to each element in the array. The callback function takes two arguments: the two elements to compare. The callback function returns a number that indicates which element should come first in the sorted array.
-
-The fifth code example uses the Array.prototype.find() method to find the first object in an array that satisfies a given condition. The Array.prototype.find() method takes a callback function as an argument and applies it to each element in the array. The callback function takes the current element as an argument and returns a boolean value that indicates whether the element satisfies the condition. If an element satisfies the condition, the find() method will return that element.
